28db18482228b754e491be6e5d3aebd841432e7d
Jason Liu <9583406+Zonrup@users.noreply.github.com>
2021-04-10 01:15:24 -0700

    retry: commit prod dir

--- README.md
+++
@@ -1,2 +0,0 @@
-# zonrup.github.io
-Github Pages website
--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    const onloads = document.getElementsByClassName("onload");
-    for (const onloadEL of onloads) {
-        onloadEL.className += " loaded";
-    }
-}
